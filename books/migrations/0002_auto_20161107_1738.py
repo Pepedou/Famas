14fe49b49a275bb0a42b60c88e6a2d24499c1417
José Luis Valencia Herrera <pepedou@gmail.com>
2016-11-07 12:02:09 -0600

    Corrected migrations.

--- books/migrations/0002_auto_20161107_1738.py
+++ books/migrations/0002_auto_20161107_1738.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='bookpage',
-            name='id',
-        ),
         migrations.AddField(
             model_name='bookpage',
             name='page_number',
