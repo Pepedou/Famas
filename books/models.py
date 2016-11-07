95bc5563635a9fa862d18a1e0563039bd1dc3cd4
José Luis Valencia Herrera <pepedou@gmail.com>
2016-11-07 08:21:12 -0600

    Added token authentication.

--- books/models.py
+++ books/models.py
@@ -1,5 +1,10 @@
 from builtins import str
+
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class BookPage(models.Model):
@@ -12,3 +17,9 @@ class BookPage(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(instance=None, created=False):
+    if created:
+        Token.objects.create(user=instance)
