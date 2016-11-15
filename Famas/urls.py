50c4cd1d09074bb4068c661e84740062ae3b7d94
José Luis Valencia Herrera <pepedou@gmail.com>
2016-11-15 13:09:26 -0600

    Changed register device URL regex to allow uppercase.

--- Famas/urls.py
+++ Famas/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^register-device-token/(?P<token>[0-9a-z]+)/$', views.register_device_token)
+    url(r'^register-device-token/(?P<token>[0-9A-Za-z]+)/$', views.register_device_token)
 ]
